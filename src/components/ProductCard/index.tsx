6a839c6838684bfb62510902b670e9d1cd6fbeba
Matheus Alves <alves.matheus@trezo.com.br>
2022-04-28 10:30:49 -0300

    hover effect 3d

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -49,34 +49,36 @@ export function ProductCard() {
               alt={product.Nome} 
             />
           </div>
-          <div className="details">
-            <span className="name">{product.Nome}</span>
-            <span className="oldPrice"><del>{currencyFormat(product.PrecoAntigo)}</del></span>
-            <span className="price">{currencyFormat(product.Preco)}</span>
-            <button>Detalhes</button>
-          </div>
-          <div className="informations">
-            <div className="info">
-              <span className="label">Marca</span>
-              <span>{product.Marca}</span>
+          <div className="animation">
+            <div className="details">
+              <span className="name">{product.Nome}</span>
+              <span className="oldPrice"><del>{currencyFormat(product.PrecoAntigo)}</del></span>
+              <span className="price">{currencyFormat(product.Preco)}</span>
+              <button>Detalhes</button>
             </div>
-            <div className="info">
-              <span className="label">Memória</span>
-              <span>{product.Memoria}</span>
+            <div className="informations">
+              <div className="info">
+                <span className="label">Marca</span>
+                <span>{product.Marca}</span>
+              </div>
+              <div className="info">
+                <span className="label">Memória</span>
+                <span>{product.Memoria}</span>
+              </div>
+              <div className="info">
+                <span className="label">Cor</span>
+                <span>{product.Cor}</span>
+              </div>
+              <div className="info">
+                <span className="label">Descrição</span>
+                <span>{product.Descrição}</span>
+              </div>
+              <a target='_blank' 
+                href={`https://api.whatsapp.com/send?phone=5524998274080&text=eu%20gostaria%20de%20saber%20sobre%20o%20produto%20${product.Nome}%20de%20${product.Memoria}%20com%20o%20pre%C3%A7o%20${currencyFormat(product.Preco)}%20,%20quais%20cores%20tem%20disponíveis?`}
+                >
+                  <BsWhatsapp /> Consultar
+              </a>
             </div>
-            <div className="info">
-              <span className="label">Cor</span>
-              <span>{product.Cor}</span>
-            </div>
-            <div className="info">
-              <span className="label">Descrição</span>
-              <span>{product.Descrição}</span>
-            </div>
-            <a target='_blank' 
-              href={`https://api.whatsapp.com/send?phone=5524998274080&text=eu%20gostaria%20de%20saber%20sobre%20o%20produto%20${product.Nome}%20de%20${product.Memoria}%20com%20o%20pre%C3%A7o%20${currencyFormat(product.Preco)}%20,%20quais%20cores%20tem%20disponíveis?`}
-              >
-                <BsWhatsapp /> Consultar
-            </a>
           </div>
         </Container>
       ))}
